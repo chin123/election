91266c6362df754fee65d5e119decd11fdfea6e2
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2017-06-18 08:26:42 +0530

    Travis CI Support

--- vote/tests.py
+++ vote/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . import views
 
 # Create your tests here.
+class TestZero(TestCase):
+    def test_lock_is_zero(self):
+        """ Lock should be 0 at the start of the program """
+        self.assertEqual(views.lock,0)
+    def test_count_is_zero(self):
+        """ Vote count should be 0 at the start of the program """
+        for i in views.names:
+            for j in views.names[i]:
+                self.assertEqual(j["count"],0)
